39530bfce6bd99d8aba4b567cf26a5ce4a406eb0
Tsmith777 <Tsmith777@ymail.com>
2025-02-15 12:59:33 -0800

    Added basic logic to markdown_to_html_node.py

--- src/markdown_to_html_node.py
+++ src/markdown_to_html_node.py
@@ -1,2 +1,12 @@
+from htmlnode import HTMLNode
+from markdown_to_blocks import markdown_to_blocks
+from block_to_block_type import block_to_block_type
+
 def markdown_to_html_node(markdown):
-    pass
+    blocks = markdown_to_blocks(markdown)
+
+    for block in blocks:
+        type = block_to_block_type(block)
+
+        if type == "paragraph":
+            block = HTMLNode()
